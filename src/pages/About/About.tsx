c2b02cb77e0ebf4211524c88756c0fa18a9b49e7
Evgenii <epokalyuk@myhalsa.ru>
2025-05-26 03:36:29 +0700

    refs 2 Апдейт перед релизом на доменное имя

--- src/pages/About/About.tsx
+++ src/pages/About/About.tsx
@@ -1,4 +1,4 @@
-import { FC } from 'react';
+import { FC, useEffect } from 'react';
 
 import { Layout } from '@components/units';
 import { Size, Tags, Text } from '@components/ui';
@@ -6,41 +6,47 @@ import { Size, Tags, Text } from '@components/ui';
 import classes from './About.module.scss';
 import { AboutMock } from './About.mock.ts';
 
-export const About: FC = () => (
-  <Layout>
-    <div className={classes.greetings_box}>
-      <Text tag={Tags.Heading} size={Size.XL}>
-        {'Привет!'}
-      </Text>
+export const About:FC = () => {
+  useEffect(() => {
+    document.title = 'About - wazzupjohnny';
+  }, []);
 
-      <Text tag={Tags.Paragraph} size={Size.LG}>
-        {
-          'Я frontend-разработчик с душой сноубордиста, который обожает скорость и свободу на склонах, ласковую абиссинскую кошку и тяжелую музыку'
-        }
-      </Text>
-    </div>
+  return (
+    <Layout>
+      <div className={classes.greetings_box}>
+        <Text tag={Tags.Heading} size={Size.XL}>
+          {'Привет!'}
+        </Text>
 
-    {AboutMock.map((item, index) => (
-      <div
-        key={index}
-        className={`${classes.row} ${index % 2 === 0 ? classes.even : classes.odd}`}
-      >
-        <img src={item.image} alt={item.title} className={classes.image} />
-
-        <div className={classes.content}>
-          <Text tag={Tags.Heading} size={Size.LG}>
-            {item.title}
-          </Text>
-
-          <Text
-            tag={Tags.Paragraph}
-            size={Size.MD}
-            className={classes.description}
-          >
-            {item.description}
-          </Text>
-        </div>
+        <Text tag={Tags.Paragraph} size={Size.LG}>
+          {
+            'Я frontend-разработчик с душой сноубордиста, который обожает скорость и свободу на склонах, ласковую абиссинскую кошку и тяжелую музыку'
+          }
+        </Text>
       </div>
-    ))}
-  </Layout>
-);
+
+      {AboutMock.map((item, index) => (
+        <div
+          key={index}
+          className={`${classes.row} ${index % 2 === 0 ? classes.even : classes.odd}`}
+        >
+          <img src={item.image} alt={item.title} className={classes.image} />
+
+          <div className={classes.content}>
+            <Text tag={Tags.Heading} size={Size.LG}>
+              {item.title}
+            </Text>
+
+            <Text
+              tag={Tags.Paragraph}
+              size={Size.MD}
+              className={classes.description}
+            >
+              {item.description}
+            </Text>
+          </div>
+        </div>
+      ))}
+    </Layout>
+  );
+};
--- src/pages/Error/Error.tsx
+++ src/pages/Error/Error.tsx
@@ -1,34 +1,30 @@
-import { FC } from 'react';
+import { FC, useEffect } from 'react';
 
-import {
-  Button,
-  Card,
-  CardFooter,
-  CardHeader,
-  Link,
-  Size,
-  Tags,
-  Text,
-  Variant,
-} from '@components/ui';
+import { Button, Card, CardFooter, CardHeader, Link, Size, Tags, Text, Variant } from '@components/ui';
 import { Urls } from '@services/Route';
 
 import classes from './Error.module.scss';
 
-export const Error: FC = () => (
-  <div className={classes.box}>
-    <Card variant={Variant.Secondary} className={classes.card}>
-      <CardHeader>
-        <Text tag={Tags.Heading} size={Size.XL}>
-          {'!@#$#%, что-то пошло не так!'}
-        </Text>
-      </CardHeader>
+export const Error:FC = () => {
+  useEffect(() => {
+    document.title = 'Ooops! - wazzupjohnny';
+  }, []);
 
-      <CardFooter className={classes.card_footer}>
-        <Link to={Urls.Home} className={classes.back_home_button}>
-          <Button wide>{'На главную страницу'}</Button>
-        </Link>
-      </CardFooter>
-    </Card>
-  </div>
-);
+  return (
+    <div className={classes.box}>
+      <Card variant={Variant.Secondary} className={classes.card}>
+        <CardHeader>
+          <Text tag={Tags.Heading} size={Size.XL}>
+            {'!@#$#%, что-то пошло не так!'}
+          </Text>
+        </CardHeader>
+
+        <CardFooter className={classes.card_footer}>
+          <Link to={Urls.Home} className={classes.back_home_button}>
+            <Button wide>{'На главную страницу'}</Button>
+          </Link>
+        </CardFooter>
+      </Card>
+    </div>
+  );
+};
