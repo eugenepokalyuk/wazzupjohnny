3474ed4d56b4141905c40190368d593fc31d1343
Evgenii <eugene.pokalyuk@gmail.com>
2025-05-29 18:22:03 +0700

    refs 2 Апдейт перед релизом на доменное имя

--- src/services/Providers/Providers.tsx
+++ src/services/Providers/Providers.tsx
@@ -1,7 +1,18 @@
-import { FC, ReactNode } from 'react';
+import { FC, PropsWithChildren } from 'react';
+import { SpeedInsights } from '@vercel/speed-insights/react';
+import { Analytics } from '@vercel/analytics/react';
 
 import { ToastProvider } from '@components/ui';
 
-export const Providers: FC<{ children: ReactNode }> = ({ children }) => {
-  return <ToastProvider>{children}</ToastProvider>;
+export const Providers:FC<PropsWithChildren> = ({ children }) => {
+  return (
+    <>
+      <ToastProvider>
+        {children}
+      </ToastProvider>
+
+      <Analytics />
+      <SpeedInsights />
+    </>
+  );
 };
