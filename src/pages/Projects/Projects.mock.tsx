c7405c16a19b543a9747386a1a6c9b06b0e7e7d8
Evgenii <eugene.pokalyuk@gmail.com>
2025-06-08 04:30:15 +0700

    refs 2 add music playlist page v0.2

--- src/pages/Projects/Projects.mock.tsx
+++ src/pages/Projects/Projects.mock.tsx
@@ -1,15 +1,14 @@
 interface ProjectItemProps {
-  url: string;
-  urlGitHub: string;
-  img?: string;
-  badges: string[];
-  title: string;
-  description: string;
+  url?:string;
+  urlGitHub:string;
+  img?:string;
+  badges:string[];
+  title:string;
+  description:string;
 }
 
-export const ProjectArray: ProjectItemProps[] = [
+export const ProjectArray:ProjectItemProps[] = [
   {
-    url: 'https://react-skilla.vercel.app/',
     urlGitHub: 'https://github.com/eugenepokalyuk/react-skilla',
     badges: ['React', 'TypeScript', 'Redux', 'Date-fns', 'SCSS'],
     title: 'Call Management System',
@@ -17,7 +16,6 @@ export const ProjectArray: ProjectItemProps[] = [
       'This project is a call management system built using React and TypeScript. It provides an interface to display, filter and interact with call records.',
   },
   {
-    url: 'https://react-mindbox-pied.vercel.app/',
     urlGitHub: 'https://github.com/eugenepokalyuk/react-mindbox',
     badges: ['React', 'TypeScript', 'Tailwind', 'Jest'],
     title: 'todos',
@@ -25,7 +23,6 @@ export const ProjectArray: ProjectItemProps[] = [
       'Список ToDo, где пользователь может управлять своими задачами. Приложение построено с использованием React, TypeScript, Tailwind CSS и localStorage для постоянного хранения данных.',
   },
   {
-    url: 'https://react-crafttech.vercel.app/',
     urlGitHub: 'https://github.com/eugenepokalyuk/react-crafttech',
     badges: ['React', 'TypeScript', 'sass', 'konva'],
     title: 'Web Paint',
@@ -33,7 +30,6 @@ export const ProjectArray: ProjectItemProps[] = [
       'Это приложение, предоставляет собой бесконечный холст, на котором пользователь может добавлять фигуры и манипулировать ими. Также есть возможность отмены действий в обе стороны и изменение заднего фона.',
   },
   {
-    url: 'https://react-madsoft.vercel.app/',
     urlGitHub: 'https://github.com/eugenepokalyuk/react-madsoft',
     badges: ['React', 'TypeScript', 'Redux', 'Tailwind', 'Formik'],
     title: 'Информационная система для проверки знаний учащихся школы.',
@@ -41,7 +37,6 @@ export const ProjectArray: ProjectItemProps[] = [
       'В школе проводятся промежуточные аттестации учащихся в формате теста.',
   },
   {
-    url: 'https://react-airnet.vercel.app/',
     urlGitHub: 'https://github.com/eugenepokalyuk/react-airnet',
     badges: ['React', 'TypeScript'],
     title: 'AirNet To-Do list',
@@ -49,7 +44,6 @@ export const ProjectArray: ProjectItemProps[] = [
       'To-Do sheet in the form of a calendar, where each day is presented as a list of tasks. Clicking on a day opens a modal window with a list of tasks. In this window you can create new tasks, delete them and mark them as completed.',
   },
   {
-    url: 'https://react-pryaniky.vercel.app/',
     urlGitHub: 'https://github.com/eugenepokalyuk/react-pryaniky',
     badges: [
       'React',
@@ -65,7 +59,6 @@ export const ProjectArray: ProjectItemProps[] = [
       'The application is a secure one-page web application that provides CRUD operations (create, read, update, delete) on data stored on the server',
   },
   {
-    url: 'https://react-personik.vercel.app/',
     urlGitHub: 'https://github.com/eugenepokalyuk/react-personik',
     badges: ['React', 'TypeScript', 'Tailwind', 'React Router v6'],
     title: 'The game of cities',
@@ -73,7 +66,6 @@ export const ProjectArray: ProjectItemProps[] = [
       'This web application is a game of cities for time. The goal of the game is to name as many real cities as possible. The application includes a game with “AI” validation of cities, animation of progress bar and display of game results.',
   },
   {
-    url: 'https://react-mtg.vercel.app/',
     urlGitHub: 'https://github.com/eugenepokalyuk/react-mtg',
     badges: ['React', 'TypeScript', 'Redux', 'Vite'],
     title: 'SPA Application with Header and Reviews for MTG',
@@ -81,7 +73,6 @@ export const ProjectArray: ProjectItemProps[] = [
       'This project is a Single Page Application (SPA) built using React (class components), Redux, TypeScript, and Vite. The application consists of a header with an image, a language dropdown, a live clock, and a main section displaying a list of reviews with pagination.',
   },
   {
-    url: 'https://react-iway.vercel.app',
     urlGitHub: 'https://github.com/eugenepokalyuk/react-iway',
     badges: [
       'React',
@@ -97,7 +88,6 @@ export const ProjectArray: ProjectItemProps[] = [
       'The Trips Management System is a React-based web application designed to manage and display trip details. It includes features such as trip listing, detailed trip views, search and filtering capabilities, and pagination.',
   },
   {
-    url: 'https://next-aceplace.vercel.app/',
     urlGitHub: 'https://github.com/eugenepokalyuk/next-aceplace',
     badges: ['Next', 'React', 'TypeScript', 'CSS/SCSS', 'DaData API', 'SSR'],
     title: 'Organization Info Finder',
@@ -105,7 +95,6 @@ export const ProjectArray: ProjectItemProps[] = [
       'This project is a Next.js application that allows users to search for organizations by their tax identification number (INN) and display detailed information about the organization. The data is fetched from the DaData API.',
   },
   {
-    url: 'https://eugenepokalyuk.github.io/static-helper-designcareer/',
     urlGitHub: 'https://github.com/eugenepokalyuk/static-helper-designcareer',
     badges: ['HTML', 'CSS', 'JavaScript'],
     title: 'Helper: как максимально быстро прокачать карьеру дизайнера',
@@ -113,7 +102,6 @@ export const ProjectArray: ProjectItemProps[] = [
       'Как максимально быстро прокачать карьеру дизайнера. Интенсив создан на основе исследования карьер ведущих дизайнеров. Топ 5 фишек для буста дизайн карьеры.',
   },
   {
-    url: 'https://eugenepokalyuk.github.io/static-helper-garantiya/',
     urlGitHub: 'https://github.com/eugenepokalyuk/static-helper-garantiya',
     badges: ['HTML', 'CSS', 'JavaScript'],
     title: 'Helper: теперь с гарантией трудоустройства',
@@ -121,7 +109,6 @@ export const ProjectArray: ProjectItemProps[] = [
       'Рассказываем как мы сделали по-настоящему прозрачную гарантию трудоустройства для дизайнеров.',
   },
   {
-    url: 'https://eugenepokalyuk.github.io/static-helper-main-page/',
     urlGitHub: 'https://github.com/eugenepokalyuk/static-helper-main-page',
     badges: ['HTML', 'CSS', 'JavaScript'],
     title: 'Helper - Платформа прокачки карьеры дизайнеров',
@@ -129,7 +116,6 @@ export const ProjectArray: ProjectItemProps[] = [
       'Прокачиваем карьеру продуктовых, веб, UX/UI и графических дизайнеров. За 3-4 месяца ты усилишь скиллы и упакуешь портфолио, так, чтобы за тебя «дрались» HR-ы.',
   },
   {
-    url: 'https://skateboarding.vercel.app/',
     urlGitHub: 'https://github.com/eugenepokalyuk/vue-skateboarding',
     badges: ['Vue 3', 'Vite', 'Tailwind', 'PostCSS', 'Vue Router'],
     title: 'Skateboarding',
@@ -137,7 +123,6 @@ export const ProjectArray: ProjectItemProps[] = [
       'SkateHut was established in 2007 by Mark Johansen-Berg following a family holiday to Florida with his wife and three sons. When his eldest son spotted a pair of Heelys in one of the theme parks, he just had to have a pair! Upon arriving back to the UK, they realised that Heelys were nowhere to be seen. Mark and his family decided to do something about it.',
   },
   {
-    url: 'https://next-generate-qr.vercel.app/',
     urlGitHub: 'https://github.com/eugenepokalyuk/next-generate-qr',
     badges: ['Next', 'React', 'TypeScript', 'ShadCN-ui'],
     title: 'Beauty QR Code Generation',
@@ -145,7 +130,6 @@ export const ProjectArray: ProjectItemProps[] = [
       'Project appears to be a Next.js web application designed for generating QR codes with customizable backgrounds. The application integrates with external APIs for both QR code generation and dynamic background image creation based on user-provided prompts.',
   },
   {
-    url: 'https://astro-blog-chi-seven.vercel.app',
     urlGitHub: 'https://github.com/eugenepokalyuk/astro-blog',
     badges: ['Astro', 'TypeScript', 'PReact', 'Alpine.js', 'Tailwind'],
     title: 'Blog',
@@ -153,24 +137,21 @@ export const ProjectArray: ProjectItemProps[] = [
       'A blog built using Astro, a modern website building framework that allows for server-side visual components and integrates with Tailwind CSS for styling.',
   },
   {
-    url: 'https://ujjo-hot-sauce.vercel.app/',
+    url: 'https://next-ujjo-hot-sauce.vercel.app/',
     urlGitHub: 'https://github.com/eugenepokalyuk/next-ujjo-hot-sauce',
     badges: ['Next', 'React', 'TypeScript', 'Tailwind'],
     title: 'Ujjo Hot Sauce',
-    description:
-      'The project includes a website on Next.js with Tailwind CSS to sell a unique product - spicy coffee sauce. Includes adaptive components: header, ticker, product cards with non-standard borders.',
+    description: 'The project includes a website on Next.js with Tailwind CSS to sell a unique product - spicy coffee sauce. Includes adaptive components: header, ticker, product cards with non-standard borders.',
   },
   {
-    url: 'https://eugenepokalyuk.github.io/react-helper-personal-account/',
     urlGitHub:
       'https://github.com/eugenepokalyuk/react-helper-personal-account',
     badges: ['React', 'Redux', 'TypeScript', 'Tailwind', 'Vite'],
-    title: "Helper. Designer's profile",
+    title: 'Helper. Designer\'s profile',
     description:
-      "Minimal React setup using TypeScript and Vite, featuring Hot Module Replacement (HMR) and ESLint rules. It supports official plugins for fast refresh with Babel or SWC and guides on expanding ESLint configurations for production. It's tailored for developers seeking a streamlined React framework with Redux, TypeScript, and Tailwind CSS integration.",
+      'Minimal React setup using TypeScript and Vite, featuring Hot Module Replacement (HMR) and ESLint rules. It supports official plugins for fast refresh with Babel or SWC and guides on expanding ESLint configurations for production. It\'s tailored for developers seeking a streamlined React framework with Redux, TypeScript, and Tailwind CSS integration.',
   },
   {
-    url: 'https://become-designer.vercel.app/',
     urlGitHub: 'https://github.com/eugenepokalyuk/helper-become-designer',
     badges: ['React', 'Redux', 'TypeScript', 'Tailwind', 'Vite'],
     title: 'Helper. Become designer',
@@ -178,7 +159,6 @@ export const ProjectArray: ProjectItemProps[] = [
       'Minimal setup for React applications with Hot Module Replacement (HMR) and ESLint rules. It supports official plugins like @vitejs/plugin-react and @vitejs/plugin-react-swc for fast refresh, and provides guidance for expanding ESLint configuration for production-ready apps',
   },
   {
-    url: 'https://eugenepokalyuk.github.io/react-pet-store/',
     urlGitHub: 'https://github.com/eugenepokalyuk/react-pet-store',
     badges: [
       'React',
@@ -194,14 +174,12 @@ export const ProjectArray: ProjectItemProps[] = [
       'Online store for pets. List of goods, shopping cart, payment form for goods, animations, map widget',
   },
   {
-    url: 'https://eugenepokalyuk.github.io/react-music-collection/',
     urlGitHub: 'https://github.com/eugenepokalyuk/react-music-collection',
     badges: ['React', 'TypeScript', 'Tailwind', 'Vite'],
     title: 'Music collection',
-    description: "List of the Yandex Music user's music collection",
+    description: 'List of the Yandex Music user\'s music collection',
   },
   {
-    url: 'https://github.com/eugenepokalyuk/react_python-gameclub',
     urlGitHub: 'https://github.com/eugenepokalyuk/react_python-gameclub',
     img: 'https://private-user-images.githubusercontent.com/111701135/307546444-5cdb353f-52ea-455d-887f-bfa021538830.png?jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJnaXRodWIuY29tIiwiYXVkIjoicmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbSIsImtleSI6ImtleTUiLCJleHAiOjE3MTcyNTE5NjYsIm5iZiI6MTcxNzI1MTY2NiwicGF0aCI6Ii8xMTE3MDExMzUvMzA3NTQ2NDQ0LTVjZGIzNTNmLTUyZWEtNDU1ZC04ODdmLWJmYTAyMTUzODgzMC5wbmc_WC1BbXotQWxnb3JpdGhtPUFXUzQtSE1BQy1TSEEyNTYmWC1BbXotQ3JlZGVudGlhbD1BS0lBVkNPRFlMU0E1M1BRSzRaQSUyRjIwMjQwNjAxJTJGdXMtZWFzdC0xJTJGczMlMkZhd3M0X3JlcXVlc3QmWC1BbXotRGF0ZT0yMDI0MDYwMVQxNDIxMDZaJlgtQW16LUV4cGlyZXM9MzAwJlgtQW16LVNpZ25hdHVyZT1lZmZkZWQxY2JmNTFmNzIyYTY2YjAyMTYzYmI0MzZhNmFmN2Q1M2EwMTM3NmMzYTU3NDJmMjA3ZTQwOTUwNmE0JlgtQW16LVNpZ25lZEhlYWRlcnM9aG9zdCZhY3Rvcl9pZD0wJmtleV9pZD0wJnJlcG9faWQ9MCJ9.j2m0ktCNS0vDTi6K38K4r-P5bMUfNZfKBJSmm4D9w80',
     badges: [
@@ -223,7 +201,6 @@ export const ProjectArray: ProjectItemProps[] = [
       'Предоставляет возможность управлять рабочими местами и проводить финансовый учет. Панель позволит сотрудникам клуба с одного рабочего компьютера устанавливать доступное время игры/работы за каждым компьютером или PlayStation 5 внутри клуба. Она также предоставит информацию о выручке за смену, сумме наличных в кассе, а также содержать базу данных сотрудников и посетителей клуба. Дополнительно, панель будет иметь базу данных с текущими товарами (напитки, еда), которые предлагаются на продажу.',
   },
   {
-    url: 'https://eugenepokalyuk.github.io/react-freetogame/',
     urlGitHub: 'https://github.com/eugenepokalyuk/react-freetogame',
     badges: [
       'React',
@@ -240,7 +217,6 @@ export const ProjectArray: ProjectItemProps[] = [
       'FreeToGame provides access to the best free-to-play multiplayer games and MMO Games while rewarding gamers with free premium in-game loot and exclusive perks.',
   },
   {
-    url: 'https://inwood-pi.vercel.app/',
     urlGitHub: 'https://github.com/eugenepokalyuk/next-inwood',
     badges: ['React', 'TypeScript', 'Next', 'Tailwind'],
     title: 'Inwood',
@@ -248,15 +224,13 @@ export const ProjectArray: ProjectItemProps[] = [
       'A stylish website featuring a responsive design with a modern UI/UX, offering an exclusive furniture collection. It integrates a product carousel, detailed item views, a special package section, and a comprehensive footer with social links and navigation. The site uses Next.js, TypeScript, and TailwindCSS.',
   },
   {
-    url: 'https://eugenepokalyuk.github.io/react-real-estate/',
     urlGitHub: 'https://github.com/eugenepokalyuk/react-real-estate',
     badges: ['React', 'TypeScript', 'css-modules'],
     title: 'Real Estate',
     description:
-      "The landing page of a company specializing in the exterior of real estate is a modern and intuitive web interface created on React. The main page focuses on the company's portfolio of projects, showing high-quality images and descriptions of implemented exterior solutions for various types of real estate",
+      'The landing page of a company specializing in the exterior of real estate is a modern and intuitive web interface created on React. The main page focuses on the company\'s portfolio of projects, showing high-quality images and descriptions of implemented exterior solutions for various types of real estate',
   },
   {
-    url: 'https://eugenepokalyuk.github.io/react-burger/',
     urlGitHub: 'https://github.com/eugenepokalyuk/react-burger',
     badges: ['React', 'TypeScript', 'Redux', 'jest', 'websocket', 'cypress'],
     title: 'Stellar Burger',
