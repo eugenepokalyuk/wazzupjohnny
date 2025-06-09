b35e4cdac2616bc65d64464df5bd09720cc2ae70
Evgenii <eugene.pokalyuk@gmail.com>
2025-06-10 01:50:24 +0700

    refs 4 update header menu

---
+++ src/pages/MusicPlaylist/components/Collection/CollectionCard/CollectionCard.tsx
@@ -0,0 +1,29 @@
+import { FC } from 'react';
+
+import { Link, Tags, Text } from '@components/ui';
+
+import classes from './CollectionCard.module.scss';
+
+interface Props {
+  playlist:number;
+  index:number;
+  image:string;
+}
+
+export const CollectionCard:FC<Props> = ({ playlist, index, image }) => (
+  <Link
+    to={`https://music.yandex.ru/iframe/playlist/mackspavlov/${playlist}`}
+    className={classes.box}
+    //@ts-ignore
+    target="_blank"
+    rel="noopener noreferrer"
+  >
+    <img src={image} alt={`Daily legends #${index + 1}`} className={classes.image} />
+
+    <div className={classes.overlay}>
+      <div className={classes.text}>
+        <Text tag={Tags.Heading}>{`Daily legends #${index + 1}`}</Text>
+      </div>
+    </div>
+  </Link>
+);
