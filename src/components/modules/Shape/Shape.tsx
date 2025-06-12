e900bb9320fa7ba44332560a5b86e97fe5fef5e2
Evgenii <eugene.pokalyuk@gmail.com>
2025-06-13 04:33:28 +0700

    refs 8.2 fixes update widget

--- src/components/modules/Shape/Shape.tsx
+++ src/components/modules/Shape/Shape.tsx
@@ -5,5 +5,9 @@ import imageCatV3Src from '@public/images/shape/cat_v3.png';
 import classes from './Shape.module.scss';
 
 export const Shape: FC = () => {
-  return <img src={imageCatV3Src} className={classes.shape} alt="" />;
+  return (
+    <div className={classes.box}>
+      <img src={imageCatV3Src} className={classes.shape} alt="" />
+    </div>
+  );
 };
