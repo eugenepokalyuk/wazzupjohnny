e3e31b81277aa8c073ce3770202c6e639355bb2b
Evgenii <eugene.pokalyuk@gmail.com>
2025-09-20 01:05:36 +0700

    refs 11.8.1 update wish list

--- src/services/Api/endpoints/GetWishList/getWishList.mock.ts
+++ src/services/Api/endpoints/GetWishList/getWishList.mock.ts
@@ -1,6 +1,9 @@
-import ImageInstaxSrc from '/public/images/wishlist/instax.jpg';
-import ImageMacSrc from '/public/images/wishlist/mac.webp';
-import ImagePS5ProSrc from '/public/images/wishlist/playstation.jpeg';
+import ImageInstax1Src from '/public/images/wishlist/instax_1.webp';
+import ImageInstax2Src from '/public/images/wishlist/instax_2.webp';
+import ImageInstax3Src from '/public/images/wishlist/instax_3.webp';
+import ImageInstax4Src from '/public/images/wishlist/instax_4.png';
+import ImageInstax5Src from '/public/images/wishlist/instax_5.webp';
+import ImageInstax6Src from '/public/images/wishlist/instax_6.webp';
 import ImageCrocsSrc from '/public/images/wishlist/crocs.jpeg';
 import ImageCrocsTanjiroSrc from '/public/images/wishlist/crocsTanjiro.jpg';
 import ImageCrocsTomiokaSrc from '/public/images/wishlist/crocsTomioka.jpg';
@@ -14,6 +17,14 @@ import ImageMagicBounce from '/public/images/wishlist/magic_bounce.jpg';
 import ImagePetKitToilet1 from '/public/images/wishlist/petkit_1.png';
 import ImagePetKitToilet2 from '/public/images/wishlist/petkit_2.png';
 import ImagePetKitToilet3 from '/public/images/wishlist/petkit_3.webp';
+import ImageGuardianofFaithBelt1 from '/public/images/wishlist/guardian_of_faith_belt_1.webp';
+import ImageGuardianofFaithBelt2 from '/public/images/wishlist/guardian_of_faith_belt_2.webp';
+import ImageGuardianofFaithBelt3 from '/public/images/wishlist/guardian_of_faith_belt_3.webp';
+import ImageGuardianofFaithBelt4 from '/public/images/wishlist/guardian_of_faith_belt_4.webp';
+import ImageGuardianofFaithStraps1 from '/public/images/wishlist/guardian_of_faith_straps_1.webp';
+import ImageGuardianofFaithStraps2 from '/public/images/wishlist/guardian_of_faith_straps_2.webp';
+import ImageGuardianofFaithStraps3 from '/public/images/wishlist/guardian_of_faith_straps_3.webp';
+import ImageGuardianofFaithStraps4 from '/public/images/wishlist/guardian_of_faith_straps_4.webp';
 
 export interface WishListItem {
   imageUrl: string | string[];
@@ -24,7 +35,14 @@ export interface WishListItem {
 
 export const wishListMock: WishListItem[] = [
   {
-    imageUrl: ImageInstaxSrc,
+    imageUrl: [
+      ImageInstax1Src,
+      ImageInstax2Src,
+      ImageInstax3Src,
+      ImageInstax4Src,
+      ImageInstax5Src,
+      ImageInstax6Src,
+    ],
     title: 'Instax mini Evo™ BROWN',
     description:
       'Хочу Instax для моментальных фотографий — это как вернуть пленочную магию, но в современном стиле!',
@@ -55,14 +73,40 @@ export const wishListMock: WishListItem[] = [
     links: ['https://www.lamoda.ru/p/rtlaed367101/shoes-crocs-sabo/'],
   },
   {
+    imageUrl: [
+      ImageGuardianofFaithBelt1,
+      ImageGuardianofFaithBelt2,
+      ImageGuardianofFaithBelt3,
+      ImageGuardianofFaithBelt4,
+    ],
+    title: 'Страж Веры Пояс',
+    description:
+      'Для качалки, я ж тебе качаюсь!\n\nСтановую тягу приседания таааам и всякое другое другое, думаю вот пояс нужен, к зиме уже буду нормальные веса брать! Голубой цвет кайф) Мой текущий обхват талии 89 см.\n\nНа момент покупки лучше уточнить не бросил ли я заниматься в зале =)',
+    links: ['https://ozon.ru/t/39wZqyN'],
+  },
+  {
+    imageUrl: [
+      ImageGuardianofFaithStraps1,
+      ImageGuardianofFaithStraps2,
+      ImageGuardianofFaithStraps3,
+      ImageGuardianofFaithStraps4,
+    ],
+    title: 'Лямки для становой тяги Веры Пояс',
+    description:
+      'Для качалки, я ж тебе качаюсь (часть 2)\n\nСтановую тягу подятгивания таааам и всякое другое другое, думаю вот лямки нужны чтобы изолировать руки там где другие группы мыщц будут работать\n\nНа момент покупки лучше уточнить не бросил ли я заниматься в зале =)',
+    links: [
+      'https://www.ozon.ru/product/lyamki-sportivnye-dlya-stanovoy-tyagi-2608747560/?_bctx=CAQQ9takAQ&at=GRt2kLqYxujr3P91SApK1BvCX3vlRKirg2rXXcv3n1J7&hs=1',
+    ],
+  },
+  {
     imageUrl: ImageEasyFeedPro,
     title: 'Автоматическая кормушка для Астры',
     description:
-      '• Задать расписание кормлений\n' +
-      '• Настроить объём порций\n' +
-      '• Общаться с питомцем благодаря двусторонней аудиосвязи\n' +
-      '• Получать уведомления о кормлении и уровне корма\n' +
-      '• Вы видите и контролируете всё — где бы ни находились',
+      '\n• Задать расписание кормлений' +
+      '\n• Настроить объём порций' +
+      '\n• Общаться с питомцем благодаря двусторонней аудиосвязи' +
+      '\n• Получать уведомления о кормлении и уровне корма' +
+      '\n• Вы видите и контролируете всё — где бы ни находились',
     links: ['https://ozon.ru/t/l7ig4kg'],
   },
   {
@@ -76,20 +120,6 @@ export const wishListMock: WishListItem[] = [
     title: 'Игрушка Magic Bounce для Астры',
     description: 'Прыгающая мышка, которой не ускользнуть',
     links: ['https://ozon.ru/t/1Gvmd1q'],
-  },
-  {
-    imageUrl: ImageMacSrc,
-    title: 'MacBook Pro',
-    description:
-      'Правда с диагональю не решил, 14 или 16 дюймов, как думаешь какой выбрать? Мощный макбук про для работки и творческих задачек. Думаю брать от чипа М4',
-    links: ['https://apple.com/macbook-pro'],
-  },
-  {
-    imageUrl: ImagePS5ProSrc,
-    title: 'PlayStation 5 Pro',
-    description:
-      'Для чилла и игр — идеальный способ расслабиться, и в нфс погонять, если будет прошка куплю руль))',
-    links: ['https://www.playstation.com/en-us/ps5/ps5-pro/'],
   },
   {
     imageUrl: [ImagePetKitToilet1, ImagePetKitToilet2, ImagePetKitToilet3],
