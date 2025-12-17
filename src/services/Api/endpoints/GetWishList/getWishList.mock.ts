3554db93929d298de516e629220a25ad6ced2e4f
Evgenii <eugene.pokalyuk@gmail.com>
2025-12-17 22:10:22 +0700

    refs 11.8.13 update wishlist

--- src/services/Api/endpoints/GetWishList/getWishList.mock.ts
+++ src/services/Api/endpoints/GetWishList/getWishList.mock.ts
@@ -1,9 +1,3 @@
-import ImageInstax1Src from '/public/images/wishlist/instax_1.webp';
-import ImageInstax2Src from '/public/images/wishlist/instax_2.webp';
-import ImageInstax3Src from '/public/images/wishlist/instax_3.webp';
-import ImageInstax4Src from '/public/images/wishlist/instax_4.png';
-import ImageInstax5Src from '/public/images/wishlist/instax_5.webp';
-import ImageInstax6Src from '/public/images/wishlist/instax_6.webp';
 import ImageSurpriseMazafaka from '/public/images/wishlist/surpriseMazafaka.jpg';
 
 export interface WishListItem {
@@ -14,19 +8,6 @@ export interface WishListItem {
 }
 
 export const wishListMock: WishListItem[] = [
-  {
-    imageUrl: [
-      ImageInstax1Src,
-      ImageInstax2Src,
-      ImageInstax3Src,
-      ImageInstax4Src,
-      ImageInstax5Src,
-      ImageInstax6Src,
-    ],
-    title: 'Instax mini Evo™ BROWN',
-    description: 'Хочу Instax для моментальных фотографий - это как вернуть пленочную магию, но в современном стиле!',
-    links: ['https://instax.com/mini_evo/en/'],
-  },
   {
     imageUrl: ImageSurpriseMazafaka,
     title: 'Денежка на весенний эпик ✨',
