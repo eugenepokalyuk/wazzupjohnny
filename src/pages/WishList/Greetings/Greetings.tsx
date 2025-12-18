46be1ab66707a4329f0115dab688fe77f1b54b8a
Evgenii <eugene.pokalyuk@gmail.com>
2025-12-18 22:30:57 +0700

    refs 11.8.13 update wishlist

--- src/pages/WishList/Greetings/Greetings.tsx
+++ src/pages/WishList/Greetings/Greetings.tsx
@@ -7,13 +7,7 @@ import classes from './Greetings.module.scss';
 export const Greetings: FC = () => (
   <div className={classes.title_box}>
     <Text tag={Tags.Heading} size={Size.XL}>
-      {'Мои желания'}
-    </Text>
-
-    <Text tag={Tags.Paragraph} size={Size.LG}>
-      {
-        'Я немного выгорел, поэтому пж не покупай ничего из списка который здесь был ранее, сделай лучше сюрприз'
-      }
+      {'Чо бы Женьке подарить? А вот список ниже обнищал, но я пополняю его)'}
     </Text>
   </div>
 );
