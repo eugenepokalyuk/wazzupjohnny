f2cc6cd252f87006f9d77eb734b1cb0f05175cb4
Evgenii <eugene.pokalyuk@gmail.com>
2025-12-18 22:32:52 +0700

    refs 11.8.13 update wishlist

--- src/services/Api/endpoints/GetWishList/getWishList.mock.ts
+++ src/services/Api/endpoints/GetWishList/getWishList.mock.ts
@@ -9,14 +9,13 @@ export interface WishListItem {
 
 export const wishListMock: WishListItem[] = [
   {
-    imageUrl:
-      'https://storage.yandexcloud.net/anki.team/iblock/3c2/3c2034a1bac0765febd998dc40c3f4f7/e8c5bdac1f1bb2bd17ff8648cbf09550.jpg',
-    title: 'Adidas Originals Puffylette✨',
+    imageUrl: 'https://storage.yandexcloud.net/anki.team/iblock/3c2/3c2034a1bac0765febd998dc40c3f4f7/e8c5bdac1f1bb2bd17ff8648cbf09550.jpg',
+    title: '#1 Adidas Originals Puffylette✨',
     description: 'красивые кроссы, у меня 28.5см нога, это 45 или 44.5 размер',
   },
   {
     imageUrl: ImageSurpriseMazafaka,
-    title: 'Денежка на весенний эпик✨',
+    title: '#10 Денежка на весенний эпик✨',
     description:
       'Секретное весеннее событие! Деньги уйдут на эпик, обещаю челюсти на полу)',
   },
