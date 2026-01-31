9d65437f0d24547b472cc4cda818f86ddbd1c529
Evgenii <eugene.pokalyuk@gmail.com>
2026-01-31 19:27:28 +0700

    refs 11.8.14 update mobile

--- src/pages/Home/components/Interview/Interview.tsx
+++ src/pages/Home/components/Interview/Interview.tsx
@@ -1,13 +1,24 @@
 import { FC } from 'react';
 
-import { Tags, Text } from '@components/ui';
+import { Color, Size, Tags, Text } from '@components/ui';
+import { Breakpoints, useMedia } from '@/utils';
 
 import classes from './Interview.module.scss';
 
-export const Interview: FC = () => (
-  <div className={classes.box}>
-    <Text tag={Tags.Heading} className={classes.title}>
-      {'Привет! Меня зовут\nЕвгений. Я занимаюсь\nфронтенд разработкой'}
-    </Text>
-  </div>
-);
+export const Interview: FC = () => {
+  const isMobile = useMedia(Breakpoints.Mobile);
+
+  return (
+    <div className={classes.box}>
+      {isMobile ? (
+        <Text tag={Tags.Heading} size={Size.XL} color={Color.Default}>
+          {'Привет!\nМеня зовут Евгений\nЯ занимаюсь фронтенд разработкой'}
+        </Text>
+      ) : (
+        <Text tag={Tags.Heading} size={Size.XXL} className={classes.title}>
+          {'Привет! Меня зовут\nЕвгений. Я занимаюсь\nфронтенд разработкой'}
+        </Text>
+      )}
+    </div>
+  );
+};
